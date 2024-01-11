5fcf10d4eb9f1a5b9b061cd3d5bada0fc1e7ab53
Vishal-Moreyeahs <vishal.pawar@moreyeahs.com>
2024-01-11 15:25:03 +0530

    Update code

--- OcrInvoice.Api/Program.cs
+++ OcrInvoice.Api/Program.cs
@@ -21,11 +21,10 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
